2a1f8a0da3d5eddd35c3d0beaafe931bf0ff6b19
programmerrakib10 <biplobjak7@gmail.com>
2023-11-18 11:02:28 +0300

    JavaScript Class 1 added

---
+++ condition.js
@@ -0,0 +1,15 @@
+// JavaScript Conditions start 
+
+let isDarkMode = true;
+if (isDarkMode == true) {
+    console.log("Dark Mode is enabled");
+} else {
+    console.log("Dark Mode is disabled")
+}
+
+let password = "paython";
+if (password == "paython") {
+    console.log("Log in Successfull")
+} else {
+    console.log("incorrect password")
+}
---
+++ variable.js
@@ -0,0 +1,31 @@
+console.log("variable testing...")
+
+// var er jonno aigula use kora better, aivabe
+
+var price, car_name, car_model;
+
+price = "$5,000 dollar "
+console.log(price)
+car_name = "Audi qz"
+console.log(car_name)
+car_model = "Audi 2023 brand new"
+console.log(car_model)
+
+// let use korci aikhane 
+
+let user_name = "Shakib Al Hasan"
+console.log(user_name)
+user_name = "Tamim Iqbal"
+console.log(user_name)
+user_name = "Lionel Messi"
+console.log(user_name)
+
+// Scop and global variable 
+// Global 
+let person_age = 70;
+console.log("global", person_age)
+// Scop 
+{
+    let person_age = 50;
+    console.log("scop",person_age)
+}
